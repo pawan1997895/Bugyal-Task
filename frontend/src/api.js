1847b755a03303eb26c24451633cbcb8d0121cab
pawan1997895 <pawanyadav199717@gmail.com>
2025-01-08 00:32:14 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://bugyal-task-api.vercel.app';
 
 export const GetAllProducts = async(search = '', page = 1, limit = 5) => {
     const url =
